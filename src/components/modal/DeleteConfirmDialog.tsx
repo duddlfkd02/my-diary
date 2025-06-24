ffeaa1037566e88889dff45d03934e7f3eb5d6ee
gimhayeong <duddlfkd02@gmail.com>
2025-06-24 17:15:06 +0900

    style&fix : 버튼 레이아웃 수정 완료

--- src/components/modal/DeleteConfirmDialog.tsx
+++ src/components/modal/DeleteConfirmDialog.tsx
@@ -32,9 +32,9 @@ const DeleteConfirmDialog = ({ onConfirm, children }: DialogProps) => {
             </AlertDialogDescription>
           </AlertDialogHeader>
         </div>
-        <AlertDialogFooter className="mt-6 flex justify-center space-x-4">
-          <AlertDialogCancel className="rounded-md bg-gray-100 px-8 py-2.5 text-black">취소</AlertDialogCancel>
-          <AlertDialogAction onClick={onConfirm} className="rounded-md bg-redAccent px-8 py-2.5 text-white">
+        <AlertDialogFooter className="mt-6 flex justify-center">
+          <AlertDialogCancel className="w-full rounded-md bg-gray-100 px-8 py-2.5 text-black">취소</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm} className="w-full rounded-md bg-redAccent px-8 py-2.5 text-white">
             삭제
           </AlertDialogAction>
         </AlertDialogFooter>
