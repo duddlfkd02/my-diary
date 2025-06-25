3b2ecd3faeea5c8c310ba4b06abd0fa089d9fdc5
gimhayeong <duddlfkd02@gmail.com>
2025-06-25 14:16:16 +0900

    fix : pwa 앱 아이콘 수정

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,6 +6,7 @@ export default function Head() {
       <meta name="theme-color" content="#98AEFF" />
       <link rel="manifest" href="/manifest.json" />
       <link rel="icon" href="/icons/icon-192x192.png" />
+      <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
     </>
   );
 }
